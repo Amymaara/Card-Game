2f53460c861e2b067fd59f75a16e19b805a33689
Amymaara <98781672+Amymaara@users.noreply.github.com>
2026-04-13 21:24:12 +0200

    discard and draw aniamtions / logic

--- Card Game/Assets/Scripts/Views/HandView.cs
+++ Card Game/Assets/Scripts/Views/HandView.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 
@@ -19,6 +20,20 @@ public class HandView : MonoBehaviour
 
         cards.Add(cardView);
         yield return UpdateCardPositions(0.2f);
+    }
+
+    public CardView RemoveCard(Card card)
+    {
+        CardView cardView = GetCardView(card);
+        if (cardView == null) return null;
+        cards.Remove(cardView);
+        StartCoroutine(UpdateCardPositions(0.15f));
+        return cardView;
+    }
+
+    private CardView GetCardView(Card card)
+    {
+        return cards.Where(cardView => cardView.Card == card).FirstOrDefault(); 
     }
 
     private IEnumerator UpdateCardPositions(float duration)
